1e3923fbc377d6108fb57fb779841dc7a425e2d9
mohak92 <tamhane.mohak@gmail.com>
2019-12-02 17:09:37 -0500

    seed data

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('L A Burger');
+INSERT INTO burgers (burger_name) VALUES ('Dallas Burger');
+INSERT INTO burgers (burger_name) VALUES ('Vegan Burger');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Crunch Burger*');
