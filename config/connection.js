13b07dd62cc9f13583b62512cd032b74b4b0c84a
mohak92 <tamhane.mohak@gmail.com>
2019-12-02 22:41:47 -0500

    deployment

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,22 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 
 // MySQL conection configurations
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "mohak1992",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Your MySQL Password",
+    database: "burgers_db"
+  });
+}
 
 // Make connection.
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
